bc0718aed4e6a532e1ccdef274801df373480710
Dopa91 <code.dopatka@gmail.com>
2024-11-21 14:21:39 +0100

    edited functions in repo

--- lib/shared/shared_preferences_repository.dart
+++ lib/shared/shared_preferences_repository.dart
@@ -3,31 +3,41 @@ import 'package:simple_beautiful_checklist_exercise/shared/database_repository.d
 
 class SharedPreferencesRepository implements DatabaseRepository {
   final SharedPreferencesAsync prefs = SharedPreferencesAsync();
+
+  List<String> _items = [];
+
   @override
-  Future<void> addItem(String item) {
-    // TODO: implement addItem
-    throw UnimplementedError();
+  Future<void> addItem(String item) async {
+    _items.add(item);
+    await saveItem();
   }
 
   @override
-  Future<void> deleteItem(int index) {
-    // TODO: implement deleteItem
-    throw UnimplementedError();
+  Future<void> deleteItem(int index) async {
+    _items.removeAt(index);
+    await saveItem();
   }
 
   @override
-  Future<void> editItem(int index, String newItem) {
-    // TODO: implement editItem
-    throw UnimplementedError();
+  Future<void> editItem(int index, String newItem) async {
+    getItems();
+    _items[index] = newItem;
+    await saveItem();
   }
 
   @override
-  Future<List<String>> getItems() {
-    // TODO: implement getItems
-    throw UnimplementedError();
+  Future<List<String>> getItems() async {
+    _items = await prefs.getStringList("toDos") ?? [];
+    return _items;
   }
 
   @override
-  // TODO: implement itemCount
-  Future<int> get itemCount => throw UnimplementedError();
+  Future<int> get itemCount async {
+    await getItems();
+    return _items.length;
+  }
+
+  Future<void> saveItem() async {
+    await prefs.setStringList("toDos", _items);
+  }
 }
